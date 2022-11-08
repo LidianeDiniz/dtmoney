68d2c60e4de4f795c794e0d1c6d67a3f1d93f56a
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-11-07 21:42:57 -0300

    Components Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { GlobalStyle } from './styles/styles.global';
+import { Header } from './components/Header';
+
 export function App() {
   return (
-    <div className="App">
-     <h1>Hello World</h1>
+    <>
+     <Header/>
      <GlobalStyle />
-    </div>
+    
+
+    </>
   );
 }
 
