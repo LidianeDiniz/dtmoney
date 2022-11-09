3f181a5f46a234d7a70924dff71a4521b7298471
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-11-08 22:46:33 -0300

    React-modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
 import { GlobalStyle } from './styles/styles.global';
+import Modal from 'react-modal'
 import { Header } from './components/Header';
 import Dashboard from './components/Dashboard/index';
+import { useState } from 'react';
 
 export function App() {
+
+  const[isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionsModal(){
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionsModal(){
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-     <Header/>
+     <Header onOpenNewTransactionModal={ handleOpenNewTransactionsModal}/>
+
      <Dashboard/>
+
+     <Modal isOpen={isNewTransactionModalOpen}
+       onRequestClose={handleCloseNewTransactionsModal}>
+        <h2>Cadastrar Transação</h2>
+       </Modal>
+
      <GlobalStyle />
     
 
