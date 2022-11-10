8ab4eb251fb01b1f3502c6dc2b1d40900d340bf4
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-11-10 19:21:39 -0300

    value formatting

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {createServer, Model} from 'miragejs';
 import {App} from './App';
-import { request } from 'http';
 
 createServer({
 
   models:{
-    trasaction : Model,
+    transaction : Model,
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title:'Freelance de website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-02-12 09:00:00'),
+        },
+
+        {
+            id: 2,
+            title: 'Aluguel',
+            type: 'withdraw',
+            category: 'Casa',
+            amount: 1100,
+            createdAt: new Date('2022-02-14 11:00:00'),
+          }
+        
+        ],
+      
+    })
   },
 
 
@@ -15,10 +40,10 @@ createServer({
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return( this.schema.all('trasaction'))
+      return this.schema.all('transaction')
     })
 
-    this.post('transactions', (schema, request) => {
+    this.post('/transactions', (schema, request) => {
       const data = JSON.parse(request.requestBody)
 
       return schema.create('transaction', data);
